2339389d5459daf913ed9b2bf2b32fba9ca22348
kim-hyunjin <hyunjin1612@gmail.com>
2021-12-03 17:37:33 +0900

    MiniCssExtractPlugin 테스트

--- src/cat.js
+++ src/cat.js
@@ -2,12 +2,15 @@ import catSrc from "../assets/cat.jpg";
 import "../styles/cat.css";
 
 export default function renderCat() {
-    const div = document.createElement("div");
-    document.body.appendChild(div);
-    div.className = "catDiv";
+    const btn = document.getElementById("catBtn");
+    btn.onclick = () => {
+        const div = document.createElement("div");
+        document.body.appendChild(div);
+        div.className = "catDiv";
 
-    const catImg = document.createElement("img");
-    catImg.src = catSrc;
-    catImg.className = "catImg";
-    div.appendChild(catImg);
+        const catImg = document.createElement("img");
+        catImg.src = catSrc;
+        catImg.className = "catImg";
+        div.appendChild(catImg);
+    };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    // mode: "development",
-    mode: "production",
+    mode: "development",
+    // mode: "production",
     entry: {
         main: "./src/app.js", // key로 사용된 main이 결과물의 이름 => 임의로 바꿔도 된다.
     },
@@ -17,22 +17,17 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.js$/, // .js 확장자로 끝나는 모든 파일
-                use: [path.resolve("./myloader.js")], // 내 로더 적용
-            },
+            // {
+            //     test: /\.js$/, // .js 확장자로 끝나는 모든 파일
+            //     use: [path.resolve("./myloader.js")], // 내 로더 적용
+            // },
             {
                 test: /\.css$/i,
                 // 로더 경로를 설정하는 대신 배열에 로더 이름을 문자열로 전달해도 된다.
                 use: [
                     MiniCssExtractPlugin.loader,
                     // "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            // url: false,
-                        },
-                    },
+                    "css-loader",
                 ], // 배열의 뒤에서부터 앞으로 로더가 동작한다.
             },
             {
@@ -48,6 +43,8 @@ module.exports = {
         ],
     },
     plugins: [
+        // dist폴더 안을 모두 삭제한 후 결과물을 만든다.(결과물에 포함되면 안되거나 불필요한 파일들을 삭제)
+        new CleanWebpackPlugin(),
         new MyPlugin(),
         // 결과물에 추가 정보를 추가할 수 있게 도와주는 플러그인
         new webpack.BannerPlugin({
@@ -61,6 +58,9 @@ module.exports = {
             VERSION: JSON.stringify("v0.0.1"),
             PRODUCTION: JSON.stringify(false),
         }),
+        // 스타일시트가 점점 많아지면 하나의 자바스크립트 결과물로 만드는 것이 부담일 수 있다.
+        // 아래 플러그인으로 css파일을 포함하는 js파일마다 별도의 CSS 파일로 만들어두어 브라우저의 리소스 다운로드 시간을 줄일 수 있다.
+        new MiniCssExtractPlugin(),
         // HTML 파일을 후처리할 수 있게 도와주는 플러그인
         new HtmlWebpackPlugin({
             minify: {
@@ -69,10 +69,5 @@ module.exports = {
             },
             hash: true, // 정적 파일을 불러올때 쿼리문자열에 웹팩 해쉬값을 추가한다
         }),
-        // dist폴더 안을 모두 삭제한 후 결과물을 만든다.(결과물에 포함되면 안되거나 불필요한 파일들을 삭제)
-        new CleanWebpackPlugin(),
-        // 스타일시트가 점점 많아지면 하나의 자바스크립트 결과물로 만드는 것이 부담일 수 있다.
-        // 아래 플러그인으로 css파일을 포함하는 js파일마다 별도의 CSS 파일로 만들어두어 브라우저의 리소스 다운로드 시간을 줄일 수 있다.
-        new MiniCssExtractPlugin(),
     ],
 };
